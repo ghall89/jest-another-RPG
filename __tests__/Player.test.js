aeb738de501423b77de39e59bd2a501beb6e42e1
Graham Hall <ghall89@me.com>
2021-03-29 18:55:55 +0000

    player health tests

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -34,3 +34,32 @@ test('get inventory from player or return false', () => {
 	
 	expect(player.getInventory()).toEqual(false);
 });
+
+test('get players health value', () => {
+	const player = new Player('Dave');
+	
+	expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+})
+
+test('checks if player is alive', () => {
+	const player = new Player('Dave');
+	
+	expect(player.isAlive()).toBeTruthy();
+	
+	player.health = 0;
+	
+	expect(player.isAlive()).toBeFalsy();
+});
+
+test('subtract from player health',  () => {
+	const player = new Player('Dave');
+	const oldHealth = player.health;
+	
+	player.reduceHealth(5);
+	
+	expect(player.health).toBe(oldHealth - 5);
+	
+	player.reduceHealth(99999);
+	
+	expect(player.health).toBe(0);
+});
